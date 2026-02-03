e91f4775b0d36c6bbf995fc30c20bb1bd3d3931c
ANURAG HALDAR <121659342+Anuraghaldar@users.noreply.github.com>
2026-02-03 23:07:29 +0530

    Time: 4 ms (60.71%) | Memory: 20.5 MB (32.61%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,17 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        i = 0
+        j = 0  
 
-        while i < len(nums):
-            if nums[i] == 0:
-                nums.pop(i)
-            else:
-                i += 1
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[j] = nums[i]
+                j += 1
 
-        for j in range(n- len(nums)):
-            nums.append(0)
+        for i in range(j, len(nums)):
+            nums[i] = 0
 
     
         
